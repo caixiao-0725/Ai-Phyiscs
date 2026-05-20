eb0947eb8825b8bf3944ba85a0259eeb9e1a36f7
cx <vegtsunami@gmail.com>
2026-05-20 15:52:46 +0800

    add render

---
+++ src/render/scene.cpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: Apache-2.0
+
+#include "scene.h"
+
+extern "C" void chysx_register_cloth_scenes();
+
+namespace chysx {
+namespace render {
+
+static std::vector<SceneEntry>& registry_impl() {
+    static std::vector<SceneEntry> r;
+    return r;
+}
+
+void register_scene(const char* name, Scene* (*factory)()) {
+    registry_impl().push_back({name, factory});
+}
+
+const std::vector<SceneEntry>& scene_registry() {
+    return registry_impl();
+}
+
+void register_all_scenes() {
+    chysx_register_cloth_scenes();
+}
+
+}  // namespace render
+}  // namespace chysx
