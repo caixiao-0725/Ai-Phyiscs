3d10510ab6b03d5f93b0928609885fa639dd0d27
cx <vegtsunami@gmail.com>
2026-05-20 16:58:57 +0800

    add scene

--- src/render/main.cu
+++ src/render/main.cu
@@ -9,7 +9,7 @@ int main(int /*argc*/, char* /*argv*/[]) {
     cfg.width = 1280;
     cfg.height = 720;
     cfg.title = "ChysX Viewer";
-    cfg.dt = 1.0f / 60.0f;
+    cfg.dt = 1.0f / 100.0f;
 
     chysx::render::Viewer viewer(cfg);
     viewer.run();
